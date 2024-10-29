1f5e2ee4c625bf6f5068e4f10eb4e28182efe978
Your Name <you@example.com>
2024-10-29 06:05:02 -0300

    🦎

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Markdown from "react-markdown";
+import rehypeHighlight from "rehype-highlight";
+import content from "./content.js";
 // import App from "./App.jsx";
 import "./index.css";
 
-const markdown = "# Hi, *Pluto*!";
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Markdown>{markdown}</Markdown>)
+    <Markdown rehypePlugins={[rehypeHighlight]}>{content}</Markdown>
   </StrictMode>,
 );
