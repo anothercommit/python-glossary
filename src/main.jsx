ecf1d455ade9a0ef9f1996b86ce5049f2a97beb2
Your Name <you@example.com>
2024-10-29 05:37:41 -0300

    🦎

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import Markdown from "react-markdown";
+// import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+const markdown = "# Hi, *Pluto*!";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <Markdown>{markdown}</Markdown>)
   </StrictMode>,
-)
+);
