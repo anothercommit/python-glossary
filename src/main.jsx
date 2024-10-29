d256a664d6dd5a3b6d3946ce645a3eee7f4c35df
Your Name <you@example.com>
2024-10-29 06:24:30 -0300

    🦎

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Markdown from "react-markdown";
 import rehypeHighlight from "rehype-highlight";
+import "highlight.js/styles/github-dark.css"; // Or any other theme
 import content from "./content.js";
 // import App from "./App.jsx";
 import "./index.css";
